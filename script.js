61afdbbd6682c9dcb4720e8ab0a0cef382cede8b
Ivan <olegkipelow@yandex.ru>
2018-12-11 00:35:07 +0700

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+a = 5;
+
+console.log(a);
+
+var number = 5;
+var string = "Hello world";
+var sym = Symbol();
+var boolean = true;
+null
+undefined
+var obj = {}
+
+console.log(4/0);
+
+let person = {
+    name: "John",
+    age: 25,
+    ismarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['bagbag.png','chuvakestgovno.jpeg','myflowuragan.bmp'];
+
+console.log(arr[0])
+
+// alert("Hello World")
+
+//let answer = confirm("На какой стул сам сядешь,на какой мать посадишь? - Обрубить пики и хуи?")
+
+//console.log(answer);
+
+//let answer = +prompt("На какой стул сам сядешь,на какой мать посадишь?")
+
+//console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+    let pisunchik = true,
+        domik = false;
+
+        console.log(pisunchik || !domik);
+
